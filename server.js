612f335b855a6947ba75a5f008c394e249c4163e
xiongkw <xiong.kw@163.com>
2018-02-24 15:28:43 +0800

    init

---
+++ server.js
@@ -0,0 +1,23 @@
+var ecstatic = require('ecstatic')
+var server = require('http').createServer(
+  ecstatic({ root: __dirname, handleError: false })
+)
+
+var io = require('socket.io')(server);
+var p2p = require('socket.io-p2p-server').Server;
+server.listen(3030);
+console.log('Listening on 3030');
+
+io.on('connection', function(socket){
+  
+  socket.on('room', function(data){
+	console.log(data.username + ' entering: ' + data.room)
+	socket.join(data.room);
+	socket.emit("msg","msg");
+	
+	p2p(socket, function () {
+		socket.emit("msg", "haha");
+    }, {name: data.room});
+  });
+  
+});
